8bfba26fcbe218b7fee90aa200d295c726c875ab
Future <futureworldzone@gmail.com>
2023-06-23 13:30:00 +0300

    if else to print Result the correct math operation

--- app.rb
+++ app.rb
@@ -13,34 +13,42 @@ puts "% to get the remainder; ** or ^ to get the exponent."
 #Storing this console input to a variable called value_operand
 is_operand_correct = false
 value_operand = gets.chomp
-puts "The mathematical operation chosen is #{value_operand}."
+msg_operation = "The mathematical operation chosen is #{value_operand}."
 
 if value_operand == "+"
     puts "Addition was chosen"
+    puts msg_operation
     is_operand_correct = true
 elsif value_operand == "-"
     puts "Subtraction was chosen"
+    puts msg_operation
     is_operand_correct = true
 elsif value_operand == "*"
     puts "Multiplication was chosen" 
+    puts msg_operation
     is_operand_correct = true
 elsif value_operand == "/"
-    puts "Division was chosen" 
+    puts "Division was chosen"
+    puts msg_operation 
     is_operand_correct = true
 elsif value_operand == "%"    
     puts "Modulus (getting a remainder) was chosen" 
+    puts msg_operation
     is_operand_correct = true
 elsif value_operand == "**"    
     puts "Exponent ** (power)was chosen" 
+    puts msg_operation
     is_operand_correct = true
 elsif value_operand == "^"    
     puts "Exponent ^ (power) was chosen" 
+    puts msg_operation
     value_operand = "**"
     is_operand_correct = true
 else 
     puts "An invalid input was entered. Try again later."
 end
 
+if is_operand_correct == true
 #Console output to ask for the second number
 puts "Please enter the second number:"
 #Storing the second console input to a float called num2
@@ -48,3 +56,6 @@ num2 = gets.chomp.to_f
 
 #Output for the float variables of num1 and num2
 puts "The numbers are #{num1} and #{num2}."
+print "Result: #{num1}#{value_operand}#{num2} = "
+puts num1.public_send(value_operand, num2)
+end
