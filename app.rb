71558a975c3c5be2226a51bb926e6995aacbbc03
Future <futureworldzone@gmail.com>
2023-06-23 12:56:58 +0300

    collecting numbers as floats from the command line

---
+++ app.rb
@@ -0,0 +1,12 @@
+#Console output to ask for the first number
+puts "Please enter the first number:"
+#Storing the first console input to a float called num1
+num1 = gets.chomp.to_f
+
+#Console output to ask for the second number
+puts "Please enter the second number:"
+#Storing the second console input to a float called num2
+num2 = gets.chomp.to_f
+
+#Output for the float variables of num1 and num2
+puts "The numbers are #{num1} and #{num2}."
