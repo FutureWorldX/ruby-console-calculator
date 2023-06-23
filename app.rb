d33543ad219e8b696bd8a0580403ca27a74c2f71
Future <futureworldzone@gmail.com>
2023-06-23 13:02:48 +0300

    Added code that stores the mathematical operation.

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
+#Console script to calculate two numbers stored as floats
+
 #Console output to ask for the first number
 puts "Please enter the first number:"
 #Storing the first console input to a float called num1
 num1 = gets.chomp.to_f
+
+#Console output to choose operations
+puts "Enter the mathematical operation that you need to perform:"
+puts "+ to add; - to subtract; * to multiply; / to divide."
+puts "% to get the remainder; ** or ^ to get the exponent."
+
+#Storing this console input to a variable called value_operand
+is_operand_correct = false
+value_operand = gets.chomp
+puts "The mathematical operation chosen is #{value_operand}."
 
 #Console output to ask for the second number
 puts "Please enter the second number:"
